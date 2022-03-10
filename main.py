a78069650b36da3c355ec2f6c303e30192067715
Manoj Dayaram <m@noj.cc>
2022-03-10 18:04:04 +0000

    Updating main.py to work when the envar is not provided.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
+import importlib
 
 # Expecting the environment variable CURRENT_CHALLENGE
 # to be set to a number corresponding to the file name
 # to run.
-current_challenge = os.environ['CURRENT_CHALLENGE'].strip()
-current_challenge = int(current_challenge)
-current_challenge = "{:02d}".format(int(current_challenge))
-
-import importlib
-importlib.import_module(current_challenge)
+current_challenge = os.getenv('CURRENT_CHALLENGE')
+try:
+  current_challenge = int(current_challenge.strip())
+  current_challenge = "{:02d}".format(int(current_challenge))
+  importlib.import_module(current_challenge)
+except:
+  print("Env variable CURRENT_CHALLENGE was incorrectly set to " + str(current_challenge))
+  print("So defaulting to running 00.py")
+  print()
+  importlib.import_module("00")
