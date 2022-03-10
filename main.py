347d97b215535a3fa86bb387a7505508cf674c40
Manoj Dayaram <m@noj.cc>
2022-03-10 18:08:29 +0000

    Ensuring errors on the import show up.

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ current_challenge = os.getenv('CURRENT_CHALLENGE')
 try:
   current_challenge = int(current_challenge.strip())
   current_challenge = "{:02d}".format(int(current_challenge))
-  importlib.import_module(current_challenge)
 except:
   print("Env variable CURRENT_CHALLENGE was incorrectly set to " + str(current_challenge))
   print("So defaulting to running 00.py")
+  current_challenge = "00"
   print()
-  importlib.import_module("00")
+
+importlib.import_module(current_challenge)
